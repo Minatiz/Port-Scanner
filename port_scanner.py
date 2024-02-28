43e480e558ca9b1b7fa5afa66d04aeb82191ff70
Alex <alex.minati1993@gmail.com>
2024-02-28 15:33:25 +0100

    Saving start work

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,24 @@
 import socket
+import nmap
 
-def get_open_ports(target, port_range, verbose = False):
+from common_ports import ports_and_services
+
+
+def get_open_ports(target, port_range, verbose=False):
+
     open_ports = []
+    ip = socket.gethostbyname(target)
+    port = '-'.join(map(str, port_range))
+    print("address:", ip, port)
+    scan = nmap.PortScanner()
+
+    return (open_ports)
 
 
+def main():
+    pass
+    # get_open_ports("", [440, 450], True)
 
-    return(open_ports)
+
+if __name__ == "__main__":
+    main()
